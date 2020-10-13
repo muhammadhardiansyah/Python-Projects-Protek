3c1be7e094059419880f132cec2604fdfe3da658
Muhammad Hardiansyah <71612996+muhammadhardiansyah@users.noreply.github.com>
2020-10-13 13:33:45 +0700

    Create latihan-5.py
    Praktikum 04

---
+++ Praktikum 04/latihan-5.py
@@ -0,0 +1,5 @@
+jumlahLakiLaki = int(input("Masukan jumlah mahasiswa laki-laki:"))
+jumlahPerempuan = int(input("Masukan jumlah mahasiswi perempuan:"))
+
+print("*"*jumlahLakiLaki)
+print("*"*jumlahPerempuan)
