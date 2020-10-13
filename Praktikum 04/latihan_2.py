3d56194659c64077fa235cd7bd03da901673785d
Muhammad Hardiansyah <71612996+muhammadhardiansyah@users.noreply.github.com>
2020-10-13 13:34:01 +0700

    Create latihan_2.py

---
+++ Praktikum 04/latihan_2.py
@@ -0,0 +1,13 @@
+print("---Nomor 2---\n")
+jarakAC = 795
+bbm = 12
+print("Diketahui:")
+print("Jarak kota A dan kota C adalah:",jarakAC,"km")
+print("jarak Konsumsi bbm setiap liter nya adalah:",bbm,"km")
+print("\nPertanyaan\nBerapa liter bensin yang dibutuhkan untuk perjalanan tersebut?\nJawab:\n")
+KonsumsiBbm = jarakAC / bbm
+
+print("=",KonsumsiBbm,"liter")
+print("\nJadi, bensin yang diperlukan untuk perjalanan tersebut adalah",KonsumsiBbm,"liter")
+
+
